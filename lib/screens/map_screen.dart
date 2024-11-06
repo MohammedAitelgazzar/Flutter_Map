34ff2d1952a8b5586dd6a22508a875ffbb34f3a5
Aitelgazzar <mohamedaitelgazzar@gmail.com>
2024-11-06 19:28:53 +0100

    search

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-const mapboxAccessToken = 'Votre token ici';
+const mapboxAccessToken =
+    'pk.eyJ1Ijoic2ltb2FpdGVsZ2F6emFyIiwiYSI6ImNtMzVzeXYyazA2bWkybHMzb2Fxb3p6aGIifQ.ORYyvkZ2Z1H8WmouDkXtvQ';
 
 final myPosition = LatLng(40.416775, -3.703790);
 
@@ -15,13 +18,37 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   final MapController _mapController = MapController();
+  final TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      _mapController.move(myPosition, 15);  
+      _mapController.move(myPosition, 15);
     });
+  }
+
+  // Fonction pour effectuer une recherche de lieu et recentrer la carte
+  Future<void> _searchLocation() async {
+    final query = _searchController.text;
+    if (query.isEmpty) return;
+
+    final url = Uri.parse(
+      'https://api.mapbox.com/geocoding/v5/mapbox.places/$query.json?access_token=$mapboxAccessToken',
+    );
+
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      if (data['features'].isNotEmpty) {
+        final location = data['features'][0]['center'];
+        final latitude = location[1];
+        final longitude = location[0];
+
+        // Recentrer la carte
+        _mapController.move(LatLng(latitude, longitude), 15);
+      }
+    }
   }
 
   @override
@@ -31,9 +58,36 @@ class _MapScreenState extends State<MapScreen> {
         centerTitle: true,
         title: const Text('simooo'),
         backgroundColor: Colors.blueAccent,
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(50.0),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                      hintText: 'Rechercher un lieu...',
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    onSubmitted: (value) => _searchLocation(),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: _searchLocation,
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
       body: FlutterMap(
-        mapController: _mapController,  
+        mapController: _mapController,
         options: MapOptions(
           minZoom: 5,
           maxZoom: 25,
